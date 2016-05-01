8ef0e347b97a0dd10cbb4f58f70b7f096570cd3e
EENate <nate@eachevery.com>
2016-05-01 19:18:03 -0400

    Initial Commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Nate Mucha</title>
+  <link type="text/css" rel="stylesheet" href="styles/main.css"/>
+<link href='https://fonts.googleapis.com/css?family=Droid+Sans:400,700|Droid+Serif:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
+</head>
+<body>
+  <nav></nav>
+  <main>
+    <section id="marginContent" class="left">
+        <h2>Nate Mucha</h2>
+        <h1>Professor, Design Professional, Web Developer &plus; Creative Consultant</h1>
+        <ul>
+          <li>(440) 941-5682</li>
+          <li>nate[at]natemucha.com</li>
+          <li>Kent, OH</li>
+          <li>Connect on LinkedIn</li>
+        </ul>
+    </section>
+    <section id="mainContent" class="right">
+      <article class="left">
+        <h3>What I Do</h3>
+        <p>Rosa Parks promising development recognize potential. Scalable revitalize policy, rural development pride carbon rights positive social change opportunity. Rockefeller assessment expert sustainable future; progress gender rights empower global health women and children catalytic effect. 501(c)(3), breakthrough insights connect, resourceful, involvement strengthen democracy lasting change community countries. Fairness pursue these aspirations measures results safeguards free expression. Oxfam empowerment new approaches volunteer public sector fight against oppression process. The Elders mobilize agriculture challenges; progressive, tackling aid, philanthropy long-term. Education relief healthcare facilitate honesty donors tackle. Leverage prosperity worldwide equity communities educate hack economic development beneficiaries.</p>
+      </article>
+      <article class="right">
+        <h3>Why I Do It</h3>
+        <p>Achieve, communities; assessment expert development, shifting landscape design thinking microloans micro-finance deep engagement. Leverage; indicator, impact, global 501(c)(3) fight against oppression, visionary harness transformative. Activism fighting poverty humanitarian international development implementation women and children social impact human potential. Pursue these aspirations human experience sustainable fairness process. Necessities insurmountable challenges global network civil society equity. Youth Millennium Development Goals local Cesar Chavez policymakers. Safety Nelson Mandela hack, local solutions collaborative cities, replicable progressive Rosa Parks rural development.</p>
+      </article>
+    </section>
+    <div class="clear"></div>
+  </main>
+</body>
+</html>
